51448d5f63198f0f1fa1213c1914e75251554571
eloghosa osagie <st.eloghosa@gmail.com>
2023-06-27 19:20:43 +0100

    [wip]:generate address

--- controllers/sender/types.go
+++ controllers/sender/types.go
@@ -1,7 +1,14 @@
 package sender
 
+import "github.com/paycrest/paycrest-protocol/services"
+
 // LoginResponse dummy comment
 type LoginResponse struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
+}
+
+
+type ReceiveAddressController struct {
+	service *services.ReceiveAddressService
 }
