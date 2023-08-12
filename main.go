4e34895ef1f0d83a14211e51ba7c9c5c299718db
Chibuotu Amadi <chibuotu@gmail.com>
2023-08-12 19:15:41 +0100

    chore: add db seed functions for dev

--- main.go
+++ main.go
@@ -25,6 +25,11 @@ func main() {
 
 	defer database.GetClient().Close()
 
+	// Seed the database
+	if err := database.SeedAll(); err != nil {
+		logger.Fatalf("database SeedAll error: %s", err)
+	}
+
 	// TODO: Continue indexing erc20 transfers which could have been impacted by a downtime
 	// we can find them by checking receive addresses with a payment order that are
 	// still unused or partial after the receive address validity period.
@@ -36,5 +41,4 @@ func main() {
 	logger.Infof("Server Running at :%v", appServer)
 
 	logger.Fatalf("%v", router.Run(appServer))
-
 }
