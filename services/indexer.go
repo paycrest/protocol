ea8dfae0e7a467f56659cef607ce21bf168a6238
Chibuotu Amadi <chibuotu@gmail.com>
2023-12-09 23:54:42 +0100

    fix: attempt fix ci pipeline

--- services/indexer.go
+++ services/indexer.go
@@ -177,7 +177,6 @@ func (s *IndexerService) IndexERC20Transfer(ctx context.Context, client types.RP
 				if transferEvent.To.Hex() == receiveAddress.Address {
 					// This is a transfer to the receive address to create an order on-chain
 					// Compare the transferred value with the expected order amount
-					// Compare the transferred value with the expected order amount
 					orderAmountInSubunit := utils.ToSubunit(paymentOrder.Amount, token.Decimals)
 					var comparisonResult = transferEvent.Value.Cmp(orderAmountInSubunit)
 
