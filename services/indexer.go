2f904380ae6a718d0fd38d02b59515e85bd7a43e
Chibuotu Amadi <chibuotu@gmail.com>
2024-10-26 02:05:42 +0100

    feat(tasks): don't index linked addresses from last indexed block

--- services/indexer.go
+++ services/indexer.go
@@ -119,12 +119,7 @@ func (s *IndexerService) IndexERC20Transfer(ctx context.Context, client types.RP
 			addresses = []common.Address{common.HexToAddress(addressToWatch)}
 			startBlock = int64(toBlock) - 5000
 		} else {
-			if strings.Contains(token.Edges.Network.Identifier, "arbitrum") {
-				startBlock = 267660300
-				toBlock = 267660400
-			} else {
-				startBlock = int64(toBlock) - 100
-			}
+			startBlock = int64(toBlock) - 100
 		}
 
 		iter, err = filterer.FilterTransfer(&bind.FilterOpts{
