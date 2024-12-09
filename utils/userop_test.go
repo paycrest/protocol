46e6c2a2f1da4edaec88047c71c496140da14f0b
Prosper <prosperauthor@gmail.com>
2024-12-09 13:41:19 +0000

    test(userop): update TestUserOp to handle eth_getUserOperationReceipt response and adjust blockNumber assertion

--- utils/userop_test.go
+++ utils/userop_test.go
@@ -73,15 +73,24 @@ func TestUserOp(t *testing.T) {
 						"result":  "0xa12db69d8d43384e9d9da5f0e9b9698c97c9a90c3447aa815d3f28daf21d3834",
 					})
 					return resp, err
-				} else if strings.Contains(string(bytes), "eth_getUserOperationByHash") {
+				} else if strings.Contains(string(bytes), "eth_getUserOperationReceipt") {
 					resp, err := httpmock.NewJsonResponse(200, map[string]interface{}{
-						"jsonrpc": "2.0",
-						"id":      1,
-						"result": map[string]interface{}{
-							"transactionHash": "0xa12db69d8d43384e9d9da5f0e9b9698c97c9a90c3447aa815d3f28daf21d3834",
-							"blockNumber":     120,
-						},
-					})
+                        "jsonrpc": "2.0",
+                        "id":      1,
+                        "result": map[string]interface{}{
+                            "transactionHash": "0xa12db69d8d43384e9d9da5f0e9b9698c97c9a90c3447aa815d3f28daf21d3834",
+                            "blockNumber":     "0x708111d", // Hexadecimal string
+                            "logs": []interface{}{
+                                map[string]interface{}{
+                                    "address": "0xYourGatewayContractAddress",
+                                    "topics": []interface{}{
+                                        "0x40ccd1ceb111a3c186ef9911e1b876dc1f789ed331b86097b3b8851055b6a137",
+                                    },
+                                    "data": "0x000000000000000000000000000000000000000000000000000000000000206f89fb9887446bc159462a4bb070d9b809f0e3474b0df07b1b664e1f19a74513fd000000000000000000000000000000000000000000000000000000000000069000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000158545041777a45466a673554774774354a44314e5a696d6d6f384b78456c683534633965417766725a6e3946534f67313341444d55655a745a43396b4a4d50344b6544636553556549564d4d576d46376d494e6d382f364674365244786a334e4836423566536b3578544e6775654f6e6f35505578307a39716b584d356b43574f475a544a4c314139617557594b6f4a6b4f59504c49513343384f6f655a6e3053644f7930616679484c7a52707a5478576131615447734b6a3343553358544778747473526c75596931456439622b73477470466b486d6d3172384f37307867776b6330757648324564703078457a6662495a3657767239755755486841674b5a65416b4259387838686b544d75575470534a693475446670705247414870446850624e51517974777845567852312f445750467137483558547946713879615171454a7867666672594d526254744c554554646333773d3d0000000000000000",
+                                },
+                            },
+                        },
+                    })
 					return resp, err
 				}
 
@@ -93,7 +102,7 @@ func TestUserOp(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotEmpty(t, transactionHash, "transactionHash should not be empty")
 		assert.NotEmpty(t, orderId, "orderId should not be empty")
-		assert.Equal(t, int64(120), blockNumber)
+		assert.Equal(t, int64(118135645), blockNumber)
 
 	})
 
