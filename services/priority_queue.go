88e71a336c299252e5ede614da2c5c2c54c5b5cf
Chibuotu Amadi <chibuotu@gmail.com>
2024-12-10 12:00:54 +0100

    feat(pq): allow slippage up to 0.5

--- services/priority_queue.go
+++ services/priority_queue.go
@@ -288,7 +288,7 @@ func (s *PriorityQueueService) AssignLockPaymentOrder(ctx context.Context, order
 		}
 
 		// TODO: make the slippage of 0.5 configurable by provider
-		if rate.Sub(order.Rate).Abs().LessThan(decimal.NewFromFloat(0.5)) {
+		if rate.Sub(order.Rate).Abs().LessThanOrEqual(decimal.NewFromFloat(0.5)) {
 			// Found a match for the rate
 			if index == 0 {
 				// Match found at index 0, perform LPOP to dequeue
