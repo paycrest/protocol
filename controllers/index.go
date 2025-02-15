e03d062a53c058469d5728c73976147a4980c2bc
Chibuotu Amadi <chibuotu@gmail.com>
2025-02-16 00:08:51 +0100

    refactor: optimize Smile Identity verification process

--- controllers/index.go
+++ controllers/index.go
@@ -727,19 +727,14 @@ func (ctrl *Controller) RequestIDVerification(ctx *gin.Context) {
 		}
 	}
 
-	// Generate Smile Identity signature
-	h := hmac.New(sha256.New, []byte(identityConf.SmileIdentityApiKey))
-	h.Write([]byte(timestamp.Format(time.RFC3339Nano)))
-	h.Write([]byte(identityConf.SmileIdentityPartnerId))
-	h.Write([]byte("sid_request"))
-
 	// Initiate KYC verification
+	smileIDSignature := getSmileIDSignature(timestamp.Format(time.RFC3339Nano))
 	res, err := fastshot.NewClient(identityConf.SmileIdentityBaseUrl).
 		Config().SetTimeout(30 * time.Second).
 		Build().POST("/v1/smile_links").
 		Body().AsJSON(map[string]interface{}{
 		"partner_id":   identityConf.SmileIdentityPartnerId,
-		"signature":    base64.StdEncoding.EncodeToString(h.Sum(nil)),
+		"signature":    smileIDSignature,
 		"timestamp":    timestamp,
 		"name":         "Aggregator KYC",
 		"company_name": "Paycrest",
@@ -755,15 +750,15 @@ func (ctrl *Controller) RequestIDVerification(ctx *gin.Context) {
 				"id_type":             "DRIVERS_LICENSE",
 				"verification_method": "doc_verification",
 			},
-			{
-				"country":             "NG",
-				"id_type":             "V_NIN",
-				"verification_method": "biometric_kyc",
-			},
+			// {
+			// 	"country":             "NG",
+			// 	"id_type":             "NIN_SLIP",
+			// 	"verification_method": "biometric_kyc",
+			// },
 			{
 				"country":             "NG",
 				"id_type":             "VOTER_ID",
-				"verification_method": "biometric_kyc",
+				"verification_method": "enhanced_document_verification",
 			},
 			{
 				"country":             "NG",
@@ -799,53 +794,53 @@ func (ctrl *Controller) RequestIDVerification(ctx *gin.Context) {
 			},
 
 			// Ghana
-			// {
-			// 	"country":             "GH",
-			// 	"id_type":             "PASSPORT",
-			// 	"verification_method": "enhanced_document_verification",
-			// },
-			// {
-			// 	"country":             "GH",
-			// 	"id_type":             "VOTER_ID",
-			// 	"verification_method": "enhanced_document_verification",
-			// },
-			// {
-			// 	"country":             "GH",
-			// 	"id_type":             "NEW_VOTER_ID",
-			// 	"verification_method": "biometric_kyc",
-			// },
-			// {
-			// 	"country":             "GH",
-			// 	"id_type":             "DRIVERS_LICENSE",
-			// 	"verification_method": "doc_verification",
-			// },
-			// {
-			// 	"country":             "GH",
-			// 	"id_type":             "SSNIT",
-			// 	"verification_method": "biometric_kyc",
-			// },
+			{
+				"country":             "GH",
+				"id_type":             "PASSPORT",
+				"verification_method": "enhanced_document_verification",
+			},
+			{
+				"country":             "GH",
+				"id_type":             "VOTER_ID",
+				"verification_method": "enhanced_document_verification",
+			},
+			{
+				"country":             "GH",
+				"id_type":             "NEW_VOTER_ID",
+				"verification_method": "biometric_kyc",
+			},
+			{
+				"country":             "GH",
+				"id_type":             "DRIVERS_LICENSE",
+				"verification_method": "doc_verification",
+			},
+			{
+				"country":             "GH",
+				"id_type":             "SSNIT",
+				"verification_method": "biometric_kyc",
+			},
 
 			// South Africa
-			// {
-			// 	"country":             "ZA",
-			// 	"id_type":             "PASSPORT",
-			// 	"verification_method": "doc_verification",
-			// },
-			// {
-			// 	"country":             "ZA",
-			// 	"id_type":             "DRIVERS_LICENSE",
-			// 	"verification_method": "doc_verification",
-			// },
-			// {
-			// 	"country":             "ZA",
-			// 	"id_type":             "RESIDENT_ID",
-			// 	"verification_method": "doc_verification",
-			// },
-			// {
-			// 	"country":             "ZA",
-			// 	"id_type":             "NATIONAL_ID",
-			// 	"verification_method": "biometric_kyc",
-			// },
+			{
+				"country":             "ZA",
+				"id_type":             "PASSPORT",
+				"verification_method": "doc_verification",
+			},
+			{
+				"country":             "ZA",
+				"id_type":             "DRIVERS_LICENSE",
+				"verification_method": "doc_verification",
+			},
+			{
+				"country":             "ZA",
+				"id_type":             "RESIDENT_ID",
+				"verification_method": "doc_verification",
+			},
+			{
+				"country":             "ZA",
+				"id_type":             "NATIONAL_ID",
+				"verification_method": "biometric_kyc",
+			},
 		},
 		"callback_url":            fmt.Sprintf("%s/v1/kyc/webhook", serverConf.HostDomain),
 		"data_privacy_policy_url": "https://paycrest.notion.site/KYC-Policy-10e2482d45a280e191b8d47d76a8d242",
@@ -876,7 +871,7 @@ func (ctrl *Controller) RequestIDVerification(ctx *gin.Context) {
 		SetPlatform("smile_id").
 		SetPlatformRef(data["ref_id"].(string)).
 		SetVerificationURL(data["link"].(string)).
-		SetLastURLCreatedAt(timestamp.Add(24 * time.Hour)).
+		SetLastURLCreatedAt(timestamp).
 		Save(ctx)
 	if err != nil {
 		logger.Errorf("error: %v", err)
@@ -913,47 +908,17 @@ func (ctrl *Controller) GetIDVerificationStatus(ctx *gin.Context) {
 		return
 	}
 
-	// Check if the status is pending and fetch the status from the platform
-	// if ivr.Status == identityverificationrequest.StatusPending {
-	// 	status, err := getSmileLinkStatus(ivr.PlatformRef)
-	// 	if err != nil {
-	// 		logger.Errorf("error: %v", err)
-	// 		u.APIResponse(ctx, http.StatusServiceUnavailable, "error", "Failed to fetch identity verification status", nil)
-	// 		return
-	// 	}
-
-	// 	response.Status = status
-
-	// 	if status != "pending" {
-	// 		// Update the verification status in the database if it's not pending
-	// 		_, err := storage.Client.IdentityVerificationRequest.
-	// 			Update().
-	// 			Where(
-	// 				identityverificationrequest.WalletAddressEQ(walletAddress),
-	// 			).
-	// 			SetStatus(identityverificationrequest.Status(response.Status)).
-	// 			Save(ctx)
-	// 		if err != nil {
-	// 			logger.Errorf("error: %v", err)
-	// 			u.APIResponse(ctx, http.StatusInternalServerError, "error", "Failed to update identity verification status", nil)
-	// 			return
-	// 		}
-	// 	}
-	// }
-
-	var status string
+	response := &types.IDVerificationStatusResponse{
+		URL:    ivr.VerificationURL,
+		Status: ivr.Status.String(),
+	}
 
 	// Check if the verification URL has expired
 	if ivr.LastURLCreatedAt.Add(1 * time.Hour).Before(time.Now()) {
-		status = "expired"
-	} else {
-		status = ivr.Status.String()
+		response.Status = "expired"
 	}
 
-	u.APIResponse(ctx, http.StatusOK, "success", "Identity verification status fetched successfully", &types.IDVerificationStatusResponse{
-		Status: status,
-		URL:    ivr.VerificationURL,
-	})
+	u.APIResponse(ctx, http.StatusOK, "success", "Identity verification status fetched successfully", response)
 }
 
 // KYCWebhook handles the webhook callback from Smile Identity
@@ -1034,61 +999,16 @@ func (ctrl *Controller) KYCWebhook(ctx *gin.Context) {
 
 // verifyWebhookSignature verifies the signature of a Smile Identity webhook
 func verifySmileIDWebhookSignature(payload types.SmileIDWebhookPayload, receivedSignature string) bool {
-	// Create HMAC
-	// Generate Smile Identity signature
-	h := hmac.New(sha256.New, []byte(identityConf.SmileIdentityApiKey))
-	h.Write([]byte(payload.Timestamp))
-	h.Write([]byte(identityConf.SmileIdentityPartnerId))
-	h.Write([]byte("sid_request"))
-
 	// Compare the computed signature with the one in the header
-	computedSignature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	computedSignature := getSmileIDSignature(payload.Timestamp)
 	return computedSignature == receivedSignature
 }
 
-// getSmileLinkStatus fetches the status of a Smile Link
-func getSmileLinkStatus(linkID string) (string, error) {
-	// Generate signature
-	timestamp := time.Now().Format(time.RFC3339Nano)
+// getSmileIDSignature generates a signature for a Smile ID request
+func getSmileIDSignature(timestamp string) string {
 	h := hmac.New(sha256.New, []byte(identityConf.SmileIdentityApiKey))
 	h.Write([]byte(timestamp))
 	h.Write([]byte(identityConf.SmileIdentityPartnerId))
 	h.Write([]byte("sid_request"))
-
-	// Get Smile Link status
-	res, err := fastshot.NewClient(identityConf.SmileIdentityBaseUrl).
-		Config().SetTimeout(30 * time.Second).
-		Build().POST(fmt.Sprintf("/v1/smile_links/%s", linkID)).
-		Body().AsJSON(map[string]interface{}{
-		"partner_id": identityConf.SmileIdentityPartnerId,
-		"signature":  base64.StdEncoding.EncodeToString(h.Sum(nil)),
-		"timestamp":  timestamp,
-	}).
-		Send()
-	if err != nil {
-		return "", fmt.Errorf("failed to get Smile Link status: %w", err)
-	}
-
-	data, err := u.ParseJSONResponse(res.RawResponse)
-	if err != nil {
-		return "", fmt.Errorf("failed to parse Smile Link response: %w", err)
-	}
-
-	totalJobs := data["total_jobs"].(float64)
-	successfulJobs := data["successful_jobs"].(float64)
-	failedJobs := data["failed_jobs"].(float64)
-
-	if failedJobs+successfulJobs < totalJobs {
-		return "pending", nil
-	}
-
-	if totalJobs == successfulJobs && failedJobs == 0 {
-		return "success", nil
-	}
-
-	if failedJobs > 0 {
-		return "failed", nil
-	}
-
-	return "", nil
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
